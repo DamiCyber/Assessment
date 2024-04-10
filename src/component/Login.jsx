4eb19b663df85988cb5cfe74cd92b87700944c70
damola ifatifo <ifatifod4@gmail.com>
2024-04-10 08:49:49 +0100

    update

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import "../assets/style/Login.css"
 import * as yup from "yup"
 import { useFormik } from 'formik'
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsTVkHrRraCfKG07b-vPlebvtyp8dqfcE",
+  authDomain: "assessment-accda.firebaseapp.com",
+  projectId: "assessment-accda",
+  storageBucket: "assessment-accda.appspot.com",
+  messagingSenderId: "689595600731",
+  appId: "1:689595600731:web:ecbcc875c3c48e0254947e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 const Login = () => {
+
   const validationSchema = yup.object({
     username: yup.string().required("Required").max(10).min(3),
     password: yup.string().matches(/^[a-zA-z0-9]{8}$/, "Enter a password with letter and number ").required("Required").max(8).min(8),
@@ -20,6 +39,7 @@ const Login = () => {
   return (
     <div>
       <div className="bigslash">
+        <button onclick={googleIn} >connect with google</button>
         <div className="to-give">
           <div className="logo">
             <h1>Laptop4Developers</h1>
